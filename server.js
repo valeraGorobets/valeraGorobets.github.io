0f291a2a8d528abe6168487d097f53e548776dc9
Valera Gorobets <Valera Gorobets>
2017-02-25 23:08:31 +0300

    changed index location

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var port = 3000;
 var app = express();
 
 //View Engine
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
